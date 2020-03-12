a82a32680a29d6b03dcedf931deb352dc9753efe
Luis Daniel Assulfi <lassulfi@gmail.com>
2020-03-11 22:08:27 -0300

    Finalizado App

--- app/src/main/java/lassulfi/com/br/randomicos/MainActivity.kt
+++ app/src/main/java/lassulfi/com/br/randomicos/MainActivity.kt
@@ -2,11 +2,27 @@ package lassulfi.com.br.randomicos
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (supportActionBar != null) supportActionBar!!.hide()
+
+        buttonRandom.setOnClickListener(this)
+        textNumber.setOnClickListener(this)
     }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.buttonRandom || v?.id == R.id.textNumber) {
+            textNumber.text = random().toString()
+        }
+    }
+
+    fun random(): Int = Random().nextInt(50) + 1
 }
